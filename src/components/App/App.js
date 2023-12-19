23a361ca37428b54358f1916ee8f164af0983f58
VitaliiKovalenko88 <75671142+VitaliiKovalenko88@users.noreply.github.com>
2023-12-19 19:24:06 +0100

    clin folders

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,29 +6,14 @@ import Filter from '../Filter/Filter';
 import ContactList from '../ContactList/ContactList';
 import css from './App.module.css';
 
-const App = () => {
-
-
-
-
-  // const deleteContact = contactId => {
-  //   setContacts(prevContacts =>
-  //     prevContacts.filter(contact => contact.id !== contactId),
-  //   );
-  // };
-
-
-
-  return (
-    <div className={css.container}>
-      <h2>PhoneBook</h2>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter />
-      <ContactList />
-    </div>)
-
-};
+const App = () =>
+  <div className={css.container}>
+    <h2>PhoneBook</h2>
+    <ContactForm />
+    <h2>Contacts</h2>
+    <Filter />
+    <ContactList />
+  </div>;
 
 
 
